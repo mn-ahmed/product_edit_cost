2373cb3f90c75186f97f3585c3a8cd77e9064e34
aravindh <aravindh@sodexis.com>
2023-03-21 13:01:32 +0530

    [ID: 28473 ] Restrict Cost Access - Initial commit

--- product_edit_cost/__manifest__.py
+++ product_edit_cost/__manifest__.py
@@ -1,16 +1,16 @@
-# Copyright 2020-2021 Sodexis
+# Copyright 2020-2023 Sodexis
 # License OPL-1 (See LICENSE file for full copyright and licensing details).
 
 {
     'name': "Product Edit Cost",
     'summary': """
         This tinyapp is used to edit product cost if using the FIFO system.""",
-    'version': "14.0.1.0.0",
+    'version': "16.0.1.0.0",
     'category': 'Sale',
     'website': "http://sodexis.com/",
     'author': "Sodexis",
     'license': 'OPL-1',
-    'installable': True,
+    'installable': False,
     'application': False,
     'depends': [
         'product',
