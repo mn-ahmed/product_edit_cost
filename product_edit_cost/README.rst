7b4f162e1c22671e75305d3de489484c9ff00393
aravindh <aravindh@sodexis.com>
2023-03-27 11:22:10 +0530

    [ID: 28473 ] Restrict Cost Access - (specs changed)

---
+++ product_edit_cost/README.rst
@@ -0,0 +1,14 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: OPL-1
+
+================
+Product Edit Cost
+================
+
+This module makes the 'Cost' field readonly using a group.
+
+Contributors
+------------
+
+* Sodexis, Inc <dev@sodexis.com>
--- product_edit_cost/__manifest__.py
+++ product_edit_cost/__manifest__.py
@@ -2,21 +2,20 @@
 # License OPL-1 (See LICENSE file for full copyright and licensing details).
 
 {
-    'name': "Product Edit Cost",
-    'summary': """
-        This tinyapp is used to edit product cost if using the FIFO system.""",
-    'version': "16.0.1.0.0",
-    'category': 'Sale',
-    'website': "http://sodexis.com/",
-    'author': "Sodexis",
-    'license': 'OPL-1',
-    'installable': True,
-    'application': False,
-    'depends': [
-        'product',
-        'mrp_account',
+    "name": "Product Edit Cost",
+    "summary": """This module protects the 'Cost' field using a group""",
+    "version": "16.0.1.0.0",
+    "category": "Sale",
+    "website": "https://sodexis.com/",
+    "author": "Sodexis",
+    "license": "OPL-1",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "product",
     ],
-    'data': [
-        'views/product_views.xml',
+    "data": [
+        "views/product_views.xml",
+        "security/ir_security.xml",
     ],
 }
